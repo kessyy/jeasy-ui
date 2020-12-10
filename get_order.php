69912b8641310e3e29f554ff0caa38d2c27274a1
kessyy <kather946@gmail.com>
2020-12-10 23:36:48 +0300

    add search

--- get_order.php
+++ get_order.php
@@ -1,22 +1,24 @@
 <?php
+	
 	$page = isset($_POST['page']) ? intval($_POST['page']) : 1;
 	$rows = isset($_POST['rows']) ? intval($_POST['rows']) : 10;
+	$description = isset($_POST['description']) ? mysqli_real_escape_string($_POST['description']) : '';
 	$offset = ($page-1)*$rows;
 	$result = array();
-
-	include 'conn.php';
 	
-	$rs = mysqli_query("select count(*) from orders");
+	include 'conn.php'
+	$where = "description like '$description%'";
+	$rs = mysqli_query("select count(*) from orders where " . $where);
 	$row = mysqli_fetch_row($rs);
 	$result["total"] = $row[0];
-	$rs = mysql_query("select * from orders limit $offset,$rows");
 	
-	$items = array();
-	while($row = mysql_fetch_object($rs)){
-		array_push($items, $row);
+	$rs = mysqli_query("select * from orders where " . $where . " limit $offset,$rows");
+	
+	$description = array();
+	while($row = mysqli_fetch_object($rs)){
+		array_push($descriptions, $row);
 	}
-	$result["rows"] = $items;
-
+	$result["rows"] = $descriptions;
+	
 	echo json_encode($result);
-
 ?>
--- search_order.php
+++ search_order.php
@@ -1,14 +1,26 @@
 <?php
 
-$id = intval($_REQUEST['description']);
+$page = isset($_POST['page']) ? intval($_POST['page']) : 1;
+$rows = isset($_POST['rows']) ? intval($_POST['rows']) : 10;
+$sort = isset($_POST['sort']) ? strval($_POST['sort']) : 'id';
+$order = isset($_POST['order']) ? strval($_POST['order']) : 'asc';
+$offset = ($page-1)*$rows;
+$result = array();
 
-include 'conn.php';
+include 'conn.php'
 
-$sql = "select * from orders where description=$description";
-$result = mysqli_query($conn, $sql);
-if ($result){
-	echo json_encode(array('success'=>true));
-} else {
-	echo json_encode(array('errorMsg'=>'Some errors occured.'));
+$fromDate= isset($_POST['from_date']) ? mysqli_real_escape_string($_POST['from_date']) : '';
+$toDate= isset($_POST['to_date']) ? mysqli_real_escape_string($_POST['to_date']) : '';
+
+$where = '';
+
+if ($from_date != null && $to_date != null) {
+    $where = "where date BETWEEN '$from_date%' AND '$to_date%'";
+} else if ($from_date != null) {
+    $where = "where date >= '$from_date%'";
+} else if ($to_date != null) {
+    $where = "where date <= '$to_date%'"; 
 }
-?>
+
+
+$rs = mysqli_query("select count(*) from table " . $where);
